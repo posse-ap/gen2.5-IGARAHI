05d716aeb876593c29443dde236feaeae3c4b83b
IGARASHI <m22y10m20.yoshikun@gmail.com>
2021-12-12 22:25:16 +0900

    quizy正解の際に青色に変化する。

---
+++ Quizy.js
@@ -0,0 +1,16 @@
+'use strict';
+// {
+//   document.getElementById('true choice').addEventListener,('click',() =>{
+//     const blue = document.getElementById('true-choice');
+//     const answerBox = document.getElementById('result-true')
+//    });
+// }
+
+
+  const seikai = document.getElementById('correct');
+
+    // クリック//
+    seikai.addEventListener('click', () =>{
+    seikai.classList.add('answer');
+    });
+  
