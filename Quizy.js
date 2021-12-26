e5cc4d7b86494a8a142e7d8b294441b74c13ddf0
IGARASHI <m22y10m20.yoshikun@gmail.com>
2021-12-26 23:34:06 +0900

    編集した

--- Quizy.js
+++ Quizy.js
@@ -1,52 +1,37 @@
 'use strict';
 
 
-const wronganswer = document.getElementsByClassName('false');
-const wrongseikai = document.getElementById('false');
-const wrongseikai2 = document.getElementById('false2');
 
-  const seikai = document.getElementById('correct');
-
-    // クリック//
-    seikai.addEventListener('click', () =>{
-    seikai.classList.add('answer');
-    seikai.classList.add('cantclick');
-    wrongseikai.classList.add('cantclick');
-    wrongseikai2.classList.add('cantclick');
-    let truebox = document.getElementById("truebox");
-  
-    truebox.style.display ="block";
-    });
 
 
     
-  for(let i = 0; i< wronganswer.length; i++){
-  console.log(i+'回目です');
-  wronganswer[i].addEventListener('click', () =>{
-    wronganswer[i].classList.add ('uncorrect');
-    });
-}
-
-
-// function clicked(){  
-
-//   let truebox = document.getElementById("truebox");
-  
-//   truebox.style.display ="block";
-
-  
-// }
-
-
-function missclicked(){
-var falsebox = document.getElementById("falsebox");
+  for( let i=1; i <=2; i++){
+    const trueanswer = document.getElementById("0-1-0");
+const wronganswer1 = document.getElementById("0-1-1");
+const wronganswer2 = document.getElementById("0-1-2");
+  wronganswer.addEventListener('click', () =>{
+    wronganswer+`${i}`.classList.add ('uncorrect');
+    trueanswer.classList.add ('answer');
+    trueanswer.classList.add('cantclick');
+    wronganswer+`${i}`.classList.add('cantclick');
+    let truebox = document.getElementById("falsebox");
+    truebox.style.display ="block";
+    let falsebox = document.getElementById("truebox");
     falsebox.style.display ="block";
-}
+    });
+  }
+
+
+  trueanswer.addEventListener('click', () =>{
+    trueanswer.classList.add ('answer');
+    wronganswer.classList.add('cantclick');
+    wronganswer.classList.add('cantclick');
+    let falsebox = document.getElementById("truebox");
+    falsebox.style.display ="block";
+    });
 
 
 
-// document.getElementsById("correct").onclick=(){
-//   document.getElementsByClassName("falsebox");
-// };
 
 
+// for
