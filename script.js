3b3bcfad337bca88162291c1f0fb296f554a378b
IGARASHI <m22y10m20.yoshikun@gmail.com>
2022-04-07 20:35:40 +0900

    webappモーダル作成中

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+console.log("test");
+
+
+
+
+// const logPost = document.getElementById('logPost');
+
+function logPost(){
+  console.log("aaa");
+  document.getElementById('mainWrapper').style.display="none";
+  document.getElementById('modal').style.display="block";
+}
+
