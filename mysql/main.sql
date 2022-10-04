69d5ceb98f442e87ea84b0db98ff7bbe430faae7
IGARASHI <m22y10m20.yoshikun@gmail.com>
2022-10-04 23:48:29 +0900

    27週目まで完了

--- mysql/main.sql
+++ mysql/main.sql
@@ -9,6 +9,48 @@ CREATE TABLE big_questions(
   prefecture_name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO big_questions(prefecture_name) VALUES ('東京');
-INSERT INTO big_questions(prefecture_name) VALUES ('広島');
-INSERT INTO big_questions(prefecture_name) VALUES ('上海');
+INSERT INTO big_questions(prefecture_name) VALUES 
+('東京'),
+('広島県');
+
+
+
+
+
+-- questions
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  big_question_id INT NOT NULL,
+  image VARCHAR(255) NOT NULL
+);
+
+INSERT INTO questions(big_question_id,image) VALUES
+(1,'takanawa.png'),
+(1,'kameido.png'),
+(2,'mukainada.png');
+
+
+
+-- choices
+
+DROP TABLE IF EXISTS choices;
+
+CREATE TABLE choices(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  question_id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  valid INT NOT NULL
+);
+
+INSERT INTO choices(question_id,name,valid) VALUES
+(1,'たかなわ',1),
+(1,'たかわ',0),
+(1,'こうわ',0),
+(2,'かめと',0),
+(2,'かめど',0),
+(2,'かめいど',1),
+(3,'むこうひら',0),
+(3,'むきひら',0),
+(3,'むかいなだ',1);
--- src/index.php
+++ src/index.php
@@ -4,12 +4,20 @@ $password = "password" ;
 try{
 $dbh = new PDO("mysql:host=db; dbname=posse; charset=utf8","$user","$password");
 
+if(isset($_GET['id'])) {
+  $number = $_GET['id']; 
+    }
 
 
-$stmt = $dbh->query('SELECT * FROM big_questions');
 
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-var_dump($result);
+$stmt = $dbh->prepare('SELECT * FROM big_questions WHERE id =:number');
+
+$stmt->bindValue(":number", $number);
+$stmt->execute();
+
+$big_question_name = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($result);
+
 
 
 $dbh = null;
@@ -19,5 +27,5 @@ $dbh = null;
 }
 
 
-
 ?>
+
